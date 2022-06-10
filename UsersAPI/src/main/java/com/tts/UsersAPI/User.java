b0559f8e9cc9a9fa361c37fde5f17ecd6d2ed831
Morgan Jones <100221540+morganjones1024@users.noreply.github.com>
2022-06-10 11:42:52 -0400

    Add files via upload

---
+++ UsersAPI/src/main/java/com/tts/UsersAPI/User.java
@@ -0,0 +1,33 @@
+package com.tts.UsersAPI;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="userprofile")
+
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "USER_ID", unique = true, nullable = false)
+    private Long id;
+	String firstName;
+	String lastName;
+	String state;
+
+}
